8ded2e8dfc473ec6bd67789905a5701e0925a2c0
RobinDeveloper <Robin@developerrobin.nl>
2024-04-08 19:22:49 +0200

    Updates

--- creative-portfolio-project/js/contentData.js
+++ creative-portfolio-project/js/contentData.js
@@ -29,8 +29,77 @@ const contentData = [
         author: "Robin Opheij",
         date: "October 3, 2023",
         htmlFileName: "works/FlippingInARoom/FlippingInARoom.html"
+    },
+    {
+        title: "Exploded Newspapers",
+        imageUrl: "works/ExplodedNewspaper/content/Output8.png",
+        author: "Robin Opheij",
+        date: "December 12, 2022",
+        htmlFileName: "works/ExplodedNewspaper/ExplodedNewspaper.html"
+    },
+    {
+        title: "Gaussian Horror",
+        imageUrl: "works/GaussianHoror/content/Unity_R8VrrKEqzk.png",
+        author: "Robin Opheij",
+        date: "December 7, 2023",
+        htmlFileName: "works/GaussianHoror/GaussianHorror.html"
+    },
+    {
+        title: "Mimicry",
+        imageUrl: "works/Mimicry/content/finalIthink - frame at 0m16s.jpg",
+        author: "Robin Opheij",
+        date: "December 7, 2023",
+        htmlFileName: "works/Mimicry/Mimicry.html"
+    },
+    {
+        title: "CLITools",
+        imageUrl: "works/CLITools/content/code.png",
+        author: "Robin Opheij",
+        date: "Ongoing",
+        htmlFileName: "works/CLITools/CLITools.html"
+    },
+    {
+        title: "The Long Now",
+        imageUrl: "works/TheLongNow/content/WhatsApp Image 2023-06-15 at 01.00.24.jpg",
+        author: "Robin Opheij",
+        date: "June 9, 2023",
+        htmlFileName: "works/TheLongNow/TheLongNow.html"
+    },
+    {
+        title: "VJShow",
+        imageUrl: "works/VJShow/content/Trimmed - frame at 3m33s.jpg",
+        author: "Robin Opheij",
+        date: "June 9, 2022",
+        htmlFileName: "works/VJShow/VjShow.html"
+    },
+    {
+        title: "PlantBDSM",
+        imageUrl: "works/PlantBDSM/content/IMG_5521.JPG",
+        author: "Robin Opheij",
+        date: "December 12, 2022",
+        htmlFileName: "works/PlantBDSM/PlantBDSM.html"
+    },
+    {
+        title: "PyramidSoundscape",
+        imageUrl: "works/PyramidSoundscape/content/frame.jpg",
+        author: "Robin Opheij",
+        date: "December 12, 2022",
+        htmlFileName: "works/PyramidSoundscape/Pyramid.html"
+    },
+    {
+        title: "Simple Synthesizer",
+        imageUrl: "works/SimpleSynthesizer/content/final.png",
+        author: "Robin Opheij",
+        date: "December 7, 2023",
+        htmlFileName: "works/SimpleSynthesizer/SimpleSynthesizer.html"
+    },
+    {
+        title: "Small scale light installation",
+        imageUrl: "works/SmallScaleLightInstialation/content/LightShort - frame at 0m0s.jpg",
+        author: "Robin Opheij",
+        date: "December 12, 2022",
+        htmlFileName: "works/SmallScaleLightInstialation/SmallLightInstialation.html"
     }
-
     // Add more content data as needed
 ];
 
